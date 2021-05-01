3ef786aa7c0f956d144835575326705888d88a6e
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-05-01 04:24:13 +0100

    Added more user authentication functionalities

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from rest_framework.response import Response
 from .models import StudentProfile
 from .serializers import RegisterSerializer, ListUserSerializer, CreateStudentProfileSerializer, \
-    ListStudentProfileSerializer
+    ListStudentProfileSerializer, UpdateStudentProfileSerializer, UpdateUserSerializer, ChangePasswordSerializer, \
+    LogoutSerializer
 
 User = get_user_model()
 
@@ -28,12 +29,46 @@ class ListUserView(generics.ListAPIView):
 # create student profile
 class StudentProfileView(generics.CreateAPIView):
     queryset = StudentProfile.objects.all()
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = CreateStudentProfileSerializer
 
 
 #list individual student profiles
 class ListStudentProfileView(generics.RetrieveAPIView):
     queryset = StudentProfile.objects.all()
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = ListStudentProfileSerializer
+
+
+# update user
+class UpdateUserView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
+
+
+#update student profiles
+class UpdateStudentProfileView(generics.UpdateAPIView):
+    queryset = StudentProfile.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateStudentProfileSerializer
+
+
+# change password view
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+
+# logout views
+class LogoutView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
