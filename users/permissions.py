eb6b045409c9daccdd6c9dd14860956f19920431
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-05-16 18:14:25 +0100

    added authourizations and permissions

--- users/permissions.py
+++ users/permissions.py
@@ -17,3 +17,8 @@ class IsOnlyAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
